884e623d002c5d727c44e06e225468366a8aaeff
Santiago Quevedo <santiago.quevedo@publicisgrroup.com>
2023-06-29 11:59:18 -0500

    Create Budget English version
    Modified order field to send it correctly

--- src/en/js/index.js
+++ src/en/js/index.js
@@ -573,18 +573,18 @@ buttonNext.addEventListener('click', async () => {
 
     if (validate === '') {
         order = {
-            enero: '1',
-            febrero: '2',
-            marzo: '3',
-            abril: '4',
-            mayo: '5',
-            junio: '6',
-            julio: '7',
-            agosto: '8',
-            septiembre: '9',
-            octubre: '10',
-            noviembre: '11',
-            diciembre: '12'
+            january: '1',
+            febreary: '2',
+            march: '3',
+            april: '4',
+            may: '5',
+            june: '6',
+            july: '7',
+            august: '8',
+            september: '9',
+            october: '10',
+            november: '11',
+            december: '12'
         };
 
         budgeting.order = order[month.toLowerCase()];
